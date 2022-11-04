867b282bd765766d321c5907f17a63d53bb89570
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-11-03 18:34:17 -0700

    First commit, blank project.

---
+++ Source/Settings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace TD_Find_Lib
+{
+	public class Settings : ModSettings
+	{
+		//Use Mod.settings.setting to refer to this setting.
+		public bool setting = true;
+
+		public void DoWindowContents(Rect wrect)
+		{
+			var options = new Listing_Standard();
+			options.Begin(wrect);
+			
+			options.CheckboxLabeled("Sample setting", ref setting);
+			options.Gap();
+
+			options.End();
+		}
+		
+		public override void ExposeData()
+		{
+			Scribe_Values.Look(ref setting, "setting", true);
+		}
+	}
+}
---
+++ Source/TD_Find_Lib.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Linq;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace TD_Find_Lib
+{
+	public class Mod : Verse.Mod
+	{
+		public static Settings settings;
+		public Mod(ModContentPack content) : base(content)
+		{
+			// initialize settings
+			settings = GetSettings<Settings>();
+		}
+
+		public override void DoSettingsWindowContents(Rect inRect)
+		{
+			base.DoSettingsWindowContents(inRect);
+			settings.DoWindowContents(inRect);
+		}
+
+		public override string SettingsCategory()
+		{
+			return "TD Find Lib";
+		}
+	}
+}
