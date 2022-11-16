ae15cb091e4757679157529b2d55ffca434cc198
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-11-16 15:37:53 -0800

    Restore General Category filter.
    Also putting some filters duplicated in subcategories e.g. stuff in items and buildings submenu.

--- Source/ListFilter/Filters/ListFilterTypes.cs
+++ Source/ListFilter/Filters/ListFilterTypes.cs
@@ -312,6 +312,34 @@ namespace TD_Find_Lib
 
 	}
 
+	enum ListCategory
+	{
+		Person,
+		Animal,
+		Item,
+		Building,
+		Natural,
+		Plant,
+		Other
+	}
+	class ListFilterCategory : ListFilterDropDown<ListCategory>
+	{
+		protected override bool FilterApplies(Thing thing)
+		{
+			switch (sel)
+			{
+				case ListCategory.Person: return thing is Pawn pawn && !pawn.NonHumanlikeOrWildMan();
+				case ListCategory.Animal: return thing is Pawn animal && animal.NonHumanlikeOrWildMan();
+				case ListCategory.Item: return thing.def.alwaysHaulable;
+				case ListCategory.Building: return thing is Building building && building.def.filthLeaving != ThingDefOf.Filth_RubbleRock;
+				case ListCategory.Natural: return thing is Building natural && natural.def.filthLeaving == ThingDefOf.Filth_RubbleRock;
+				case ListCategory.Plant: return thing is Plant;
+				case ListCategory.Other: return !(thing is Pawn) && !(thing is Building) && !(thing is Plant) && !thing.def.alwaysHaulable;
+			}
+			return false;
+		}
+	}
+
 	// This includes most things but not minifiable buildings.
 	public class ListFilterItemCategory : ListFilterDropDown<ThingCategoryDef>
 	{
