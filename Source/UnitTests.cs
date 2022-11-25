ae8c7972f67206fae83559dfae5af6f9617a5eda
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-11-24 17:44:26 -0800

    Unittests for fun

---
+++ Source/UnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace TD_Find_Lib
+{
+	public static class UnitTests
+	{
+		public static void Run()
+		{
+			// I dunno how this'll work but hey, it'll probably throw errors on failure.
+
+			foreach (var group in Mod.settings.searchGroups)
+				foreach (var search in group)
+					search.CloneForUse();
+
+			QuerySearch searchTest = new();
+			foreach(var queryDef in DefDatabase<ThingQueryDef>.AllDefsListForReading)
+			{
+				var query = ThingQueryMaker.MakeQuery(queryDef);
+				searchTest.Children.Add(query, remake: Find.CurrentMap != null);
+			}
+		}
+	}
+}
