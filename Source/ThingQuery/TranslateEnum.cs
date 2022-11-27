77249292d238560c7da1fbeb9cb98538ad540f4a
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-11-26 23:11:19 -0800

    Why not cache enum translation.

--- Source/ThingQuery/TranslateEnum.cs
+++ Source/ThingQuery/TranslateEnum.cs
@@ -8,8 +8,20 @@ namespace TD_Find_Lib
 {
 	public static class TranslateEnumEx
 	{
+		private static Dictionary<object, string> _enumStringCache = new();
+
 		public static string TranslateEnum(this object e)
 		{
+			if (_enumStringCache.TryGetValue(e, out string tr))
+				return tr;
+
+			string translated = e.DoTranslateEnum();
+			_enumStringCache[e] = translated;
+			return translated;
+		}
+
+		public static string DoTranslateEnum(this object e)
+		{ 
 			string type = e.GetType().Name;
 			string name = e.ToString();
 			string key = ("TD." + type + "." + name);
