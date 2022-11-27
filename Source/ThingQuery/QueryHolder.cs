f6e98bda13528f88c1734a205d0a29133942c44f
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-11-27 14:29:11 -0800

    Force save Any/all because the default is different in different contexts

--- Source/ThingQuery/QueryHolder.cs
+++ Source/ThingQuery/QueryHolder.cs
@@ -28,7 +28,7 @@ namespace TD_Find_Lib
 		public void ExposeData()
 		{
 			Scribe_Collections.Look(ref queries, "queries");
-			Scribe_Values.Look(ref matchAllQueries, "matchAllQueries", true);
+			Scribe_Values.Look(ref matchAllQueries, "matchAllQueries", forceSave: true);	//Force save because the default is different in different contexts
 
 			if(Scribe.mode == LoadSaveMode.LoadingVars)
 				foreach (var f in queries)
