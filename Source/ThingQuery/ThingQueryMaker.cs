9cba5eda06c534c01739d65a18e4b7a6e3af8743
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-11-30 00:59:55 -0800

    No need to include all the DefOfs with a new Generic method to MakeQuery from ThingQuery type

--- Source/ThingQuery/ThingQueryMaker.cs
+++ Source/ThingQuery/ThingQueryMaker.cs
@@ -8,68 +8,17 @@ using RimWorld;
 
 namespace TD_Find_Lib
 {
-	[DefOf]
+	// Create a ThingQuery in code wth ThingQueryMaker.MakeQuery<ThingQueryWhatever>();
+
 	[StaticConstructorOnStartup]
 	public static class ThingQueryMaker
 	{
-		public static ThingQueryDef Query_Area;
-		public static ThingQueryDef Query_AreaRestriction;
-		public static ThingQueryDef Query_Capacity;
-		public static ThingQueryDef Query_ClassType;
-		public static ThingQueryDef Query_Crop;
-		public static ThingQueryDef Query_Def;
-		public static ThingQueryDef Query_Designation;
-		public static ThingQueryDef Query_Deterioration;
-		public static ThingQueryDef Query_DiesLeafless;
-		public static ThingQueryDef Query_Door;
-		public static ThingQueryDef Query_Drafted;
-		public static ThingQueryDef Query_DrawerType;
-		public static ThingQueryDef Query_Egg;
-		public static ThingQueryDef Query_Faction;
-		public static ThingQueryDef Query_Fogged;
-		public static ThingQueryDef Query_Forbidden;
-		public static ThingQueryDef Query_Freshness;
-		public static ThingQueryDef Query_Gender;
-		public static ThingQueryDef Query_AndOrGroup;
-		public static ThingQueryDef Query_Growth;
-		public static ThingQueryDef Query_Guest;
-		public static ThingQueryDef Query_HP;
-		public static ThingQueryDef Query_Harvestable;
-		public static ThingQueryDef Query_Health;
-		public static ThingQueryDef Query_Incapable;
-		public static ThingQueryDef Query_Inspiration;
-		public static ThingQueryDef Query_Inventory;
-		public static ThingQueryDef Query_ItemCategory;
-		public static ThingQueryDef Query_Job;
-		public static ThingQueryDef Query_Leather;
-		public static ThingQueryDef Query_ThingQueryDevelopmentalStage;
-		public static ThingQueryDef Query_Meat;
-		public static ThingQueryDef Query_MentalState;
-		public static ThingQueryDef Query_Milk;
-		public static ThingQueryDef Query_Mineable;
-		public static ThingQueryDef Query_MissingBodyPart;
-		public static ThingQueryDef Query_Mod;
-		public static ThingQueryDef Query_Name;
-		public static ThingQueryDef Query_Nearby;
-		public static ThingQueryDef Query_Need;
-		public static ThingQueryDef Query_Onscreen;
-		public static ThingQueryDef Query_Prisoner;
-		public static ThingQueryDef Query_ProductProgress;
-		public static ThingQueryDef Query_Quality;
-		public static ThingQueryDef Query_RaceProps;
-		public static ThingQueryDef Query_Skill;
-		public static ThingQueryDef Query_SpecialFilter;
-		public static ThingQueryDef Query_Stat;
-		public static ThingQueryDef Query_Stuff;
-		public static ThingQueryDef Query_Temp;
-		public static ThingQueryDef Query_Thought;
-		public static ThingQueryDef Query_TimeToRot;
-		public static ThingQueryDef Query_Trait;
-		public static ThingQueryDef Query_Wool;
-		public static ThingQueryDef Query_Zone;
-
 		// The result is to be added to a IQueryHolder with Add()
 		// (Probably a QuerySearch or a ThingQueryGrouping)
+		private static readonly Dictionary<Type, ThingQueryDef> defForQueryClasses = new();
+		public static T MakeQuery<T>() where T : ThingQuery =>
+			(T)MakeQuery(defForQueryClasses[typeof(T)]);
+
 		public static ThingQuery MakeQuery(ThingQueryDef def)
 		{
 			ThingQuery query = (ThingQuery)Activator.CreateInstance(def.queryClass);
@@ -86,6 +35,9 @@ namespace TD_Find_Lib
 			foreach (var listDef in DefDatabase<ThingQueryCategoryDef>.AllDefs)
 				foreach (var subDef in listDef.SubQueries)
 					rootSelectableQueries.Remove(subDef);
+
+			foreach (var queryDef in DefDatabase<ThingQueryDef>.AllDefsListForReading)
+				defForQueryClasses[queryDef.queryClass] = queryDef;
 		}
 
 		public static IEnumerable<ThingQuerySelectableDef> SelectableList =>
