5757ddb4f160c3eb3133a34da848186ec4768181
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-12-13 22:26:04 -0800

    LTOColonyGroups filter. Yay.

---
+++ ModSupport/LTOColonyGroups/ThingQueryLTOGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TD_Find_Lib;
+using Verse;
+using TacticalGroups;
+
+namespace LTOColonyGroupsSupport
+{
+	public class ThingQueryLTOGroup : ThingQueryDropDown<ColonistGroup>
+	{
+		public override bool AppliesDirectlyTo(Thing thing) =>
+			sel == null ? thing is Pawn pawn && pawn.TryGetGroups(out HashSet<ColonistGroup> groups) && groups.Count(g => g is PawnGroup) == 0
+			: sel.pawns.Contains(thing);
+
+		public override string NameFor(ColonistGroup o) => o.curGroupName;
+
+		protected override ColonistGroup ResolveRef(Map map) =>
+			TacticUtils.AllPawnGroups.FirstOrDefault(g => g.curGroupName == selName);
+
+		public override string NullOption() => "None".Translate();
+
+		public override IEnumerable<ColonistGroup> Options() =>
+			TacticUtils.AllPawnGroups;
+	}
+}
