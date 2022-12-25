9638a81e3fe1e08f938e8141919f83a08e7be494
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-12-24 19:16:12 -0800

    Support Mod - Real Fog Of War.
    Tweak some LTO Names.

--- ModSupport/RealFogOfWar/Properties/AssemblyInfo.cs
+++ ModSupport/RealFogOfWar/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TDFindLib_LTOColonyGroups")]
+[assembly: AssemblyTitle("TDFindLib_RealFOW")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TDFindLib_LTOColonyGroups")]
+[assembly: AssemblyProduct("TDFindLib_RealFOW")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ ModSupport/RealFogOfWar/ThingQueryRealFOW.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TD_Find_Lib;
+using Verse;
+using RimWorldRealFoW.Utils;
+
+namespace TDFindLib_RealFOW
+{
+	public class ThingQueryRealFOW : ThingQuery
+	{
+		public override bool AppliesDirectlyTo(Thing thing) =>
+			thing.fowIsVisible();
+	}
+}
