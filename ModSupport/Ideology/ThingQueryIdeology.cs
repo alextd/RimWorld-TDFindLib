86ba71c6d023d80d81a0402bf065f7ab86da6417
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2023-07-18 16:26:06 -0700

    Initial Ideology project

---
+++ ModSupport/Ideology/ThingQueryIdeology.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TD_Find_Lib;
+using Verse;
+
+namespace TDFindLib_Ideology
+{
+	public class ThingQueryIdeology : ThingQuery
+	{
+		public override bool AppliesDirectlyTo(Thing thing) =>
+			true;
+	}
+}
