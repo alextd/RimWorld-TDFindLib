95d55a85529fcb6783e57f3ad7ddc44ce43c9db1
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2023-08-09 16:50:04 -0700

    Biotech Project

---
+++ ModSupport/Biotech/ThingQueryBiotech.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TD_Find_Lib;
+using Verse;
+
+namespace TDFindLib_Biotech
+{
+	public class ThingQueryBiotech : ThingQuery
+	{
+		public override bool AppliesDirectlyTo(Thing thing) =>
+			true;
+	}
+}
