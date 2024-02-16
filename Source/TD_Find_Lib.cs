5e2678867b6f2da0f174f9fa91647d3a80a97655
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2024-02-15 17:02:34 -0800

    Load settings in SCOS. Order mod after HAR.
    Because: Previous loading settings in ExecuteWhileFinished would happen before SCOS Queued event, and Maybe we want to run after mod's SCOS, such as HAR.

--- Source/TD_Find_Lib.cs
+++ Source/TD_Find_Lib.cs
@@ -9,26 +9,31 @@ using UnityEngine;
 
 namespace TD_Find_Lib
 {
+	[StaticConstructorOnStartup]
+	public static class ModStartup
+	{
+		static ModStartup()
+		{
+			// initialize settings, wait for defs and anything to load.
+			Mod.settings = LoadedModManager.GetMod<Mod>().GetSettings<Settings>();
+
+			if (Mod.settings.firstUse)
+			{
+				List<SearchGroup> groups = DefaultSearches.CopyLibrary;
+
+				Mod.settings.firstUse = false;
+				foreach (var group in groups)
+					Mod.settings.Add(group);
+				Mod.settings.Write();
+			}
+		}
+	}
+
 	public class Mod : Verse.Mod
 	{
 		public static Settings settings;
 		public Mod(ModContentPack content) : base(content)
 		{
-			// initialize settings, wait for defs and anything to load.
-			LongEventHandler.ExecuteWhenFinished(() =>
-			{
-				settings = GetSettings<Settings>();
-
-				if (settings.firstUse)
-				{
-					List<SearchGroup> groups = DefaultSearches.CopyLibrary;
-
-					settings.firstUse = false;
-					foreach(var group in groups)
-						settings.Add(group);
-					settings.Write();
-				}
-			});
 		}
 
 		public override void DoSettingsWindowContents(Rect inRect)
@@ -42,4 +47,5 @@ namespace TD_Find_Lib
 			return "TD.TDFindLibrary".Translate();
 		}
 	}
+
 }
