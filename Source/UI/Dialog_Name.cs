3d4214d977a4515c132290b170e0a4d54bb653bd
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2024-03-14 02:57:54 -0700

    base Dialog_Rename now uses template, so I'm copying old code and using that
    TODO: use vanilla renamer when appropriate

--- Source/UI/Dialog_Name.cs
+++ Source/UI/Dialog_Name.cs
@@ -3,42 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Verse;
+using RimWorld;
 using UnityEngine;
 
 namespace TD_Find_Lib
 {
-	public class Dialog_Name : Dialog_Rename
+	// TOOD: Use 1.5 Dialog_Rename where approrpriate
+	// Some places were actual renames, 
+	// Some where making new objects with a given name.
+	public class Dialog_Name : Window
 	{
+		protected string curName;
+
+		private bool focusedRenameField;
+
+		public int MaxNameLength => 256;
+
 		const float TitleHeight = 35f;
 
 		Action<string> setNameAction;
 		Predicate<string> rejector;
 		string title;
 
-		//actually protected
-		public override int MaxNameLength => 256;
-
 		public Dialog_Name(string name, Action<string> act, string title = null, Predicate<string> rejector = null)
 		{
+			forcePause = true;
+			doCloseX = true;
+			absorbInputAroundWindow = true;
+			closeOnAccept = false;
+			closeOnClickedOutside = true;
+
 			curName = name;
 			setNameAction = act;
 			this.title = title;
 			this.rejector = rejector;
 		}
 
-		//protected but using publicized assembly
-		//protected override void SetName(string name)
-		public override void SetName(string name)
+		public AcceptanceReport NameIsValid(string name)
 		{
-			setNameAction(name);
-		}
-
-		public override AcceptanceReport NameIsValid(string name)
-		{
-			AcceptanceReport result = base.NameIsValid(name);
-			if (!result.Accepted)
+			if (name.Length == 0)
 			{
-				return result;
+				return false;
 			}
 
 			if (rejector != null && rejector(name))
@@ -53,7 +58,7 @@ namespace TD_Find_Lib
 		{
 			if (title == null)
 			{
-				base.DoWindowContents(inRect);
+				DoWindowContents2(inRect);
 				return;
 			}
 
@@ -63,8 +68,51 @@ namespace TD_Find_Lib
 			inRect.yMin += TitleHeight;
 
 			GUI.BeginGroup(inRect);
-			base.DoWindowContents(inRect.AtZero());
+			DoWindowContents2(inRect.AtZero());
 			GUI.EndGroup();
+		}
+
+		public  void DoWindowContents2(Rect inRect)
+		{
+			Text.Font = GameFont.Small;
+			bool flag = false;
+			if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Return)
+			{
+				flag = true;
+				Event.current.Use();
+			}
+			GUI.SetNextControlName("RenameField");
+			string text = Widgets.TextField(new Rect(0f, 15f, inRect.width, 35f), curName);
+			if (text.Length < MaxNameLength)
+			{
+				curName = text;
+			}
+			if (!focusedRenameField)
+			{
+				UI.FocusControl("RenameField", this);
+				focusedRenameField = true;
+			}
+			if (!(Widgets.ButtonText(new Rect(15f, inRect.height - 35f - 15f, inRect.width - 15f - 15f, 35f), "OK") || flag))
+			{
+				return;
+			}
+			AcceptanceReport acceptanceReport = NameIsValid(curName);
+			if (!acceptanceReport.Accepted)
+			{
+				if (acceptanceReport.Reason.NullOrEmpty())
+				{
+					Messages.Message("NameIsInvalid".Translate(), MessageTypeDefOf.RejectInput, historical: false);
+				}
+				else
+				{
+					Messages.Message(acceptanceReport.Reason, MessageTypeDefOf.RejectInput, historical: false);
+				}
+			}
+			else
+			{
+				setNameAction(curName);
+				Find.WindowStack.TryRemove(this);
+			}
 		}
 
 	}
