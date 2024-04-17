78f398f49505159a8d7c07ddf87cb32cc100b4cc
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2024-04-17 14:24:35 -0700

    Add support for CAI Fog Of War

---
+++ ModSupport/CAIFOW/ThingQueryCAIFOW.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TD_Find_Lib;
+using Verse;
+using CombatAI;
+
+namespace TDFindLib_CAIFOW
+{
+	public class ThingQueryCAIFOW : ThingQuery
+	{
+		public override bool AppliesDirectlyTo(Thing thing) =>
+			Finder.Settings.FogOfWar_Enabled
+			&& !(thing.MapHeld.GetComp_Fast<MapComponent_FogGrid>()?.IsFogged(thing.PositionHeld) ?? false); // default false => not fogged => return true
+	}
+}
